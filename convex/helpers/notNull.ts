9f40151216df99c93f0cee2e4a40fd51cc8306fc
Mike Wheaton <mike@wheaton.dev>
2022-10-25 17:14:02 -0500

    Simplify export

--- convex/helpers/notNull.ts
+++ convex/helpers/notNull.ts
@@ -1,6 +1,4 @@
 // A custom typeguard that can be used to filter null values out of an array.
-function notNull<T>(val: T | null): val is T {
+export default function notNull<T>(val: T | null): val is T {
   return val !== null;
 }
-
-export default notNull;
